339ce5df0cae15b25b9e9dc155ce935aaa9d18ef
ACleverMan <ACleverMan@users.noreply.github.com>
2018-12-22 14:44:01 -0500

    GridMaker

---
+++ FireEmblemGame/Assets/_Scripts/GridMaker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GridMaker : MonoBehaviour
+{
+    public float gridSize;
+        
+    private int hCells, vCells;
+
+    public GameObject bottomLeft, topRight;
+
+    public GameObject tilePrefab, tileHolder;
+
+    bool active;
+
+    public void make()
+    {
+        if (active)
+            return;
+        else
+        {
+            active = true;
+
+            hCells = (int)(Math.Abs(bottomLeft.transform.position.x - topRight.transform.position.x) / gridSize);
+            vCells = (int)(Math.Abs(bottomLeft.transform.position.y - topRight.transform.position.y) / gridSize);
+
+            for (int i = 0; i < hCells; i++)
+            {
+                for (int j = 0; j < vCells; j++)
+                {
+                    GameObject tile = Instantiate(tilePrefab, new Vector3(bottomLeft.transform.position.x + i * gridSize, bottomLeft.transform.position.y + j * gridSize, bottomLeft.transform.position.z), Quaternion.identity, tileHolder.transform) as GameObject;
+                }
+            }
+
+        }
+
+    }
+
+
+}
---
+++ FireEmblemGame/Assets/_Scripts/GridMakerEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridMaker))]
+[CanEditMultipleObjects]
+public class GridMakerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GridMaker maker = (GridMaker)target;
+        if (GUILayout.Button("make grid"))
+        {
+            maker.make();
+        }
+    }
+}
